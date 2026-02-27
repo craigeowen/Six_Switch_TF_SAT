14902443032ba5f3d11bdd4a0f9f77b57991ef3d
craigeowen <craigeowen@gmail.com>
2026-02-27 17:14:00 +0000

    com2

--- main.tf
+++ main.tf
@@ -13,24 +13,24 @@ locals {
 
 
 # Configure the Cisco NX-OS provider
-provider "nxos" {
-  alias    = "sat01"
-  username = var.nxos_username
-  password = var.nxos_password
-  url      = var.switches.sat01_url
-}
+# provider "nxos" {
+#   alias    = "sat01"
+#   username = var.nxos_username
+#   password = var.nxos_password
+#   url      = var.switches.sat01_url
+# }
 
-provider "nxos" {
-  alias    = "sat02"
-  username = var.nxos_username
-  password = var.nxos_password
-  url      = var.switches.sat02_url
-}
+# provider "nxos" {
+#   alias    = "sat02"
+#   username = var.nxos_username
+#   password = var.nxos_password
+#   url      = var.switches.sat02_url
+# }
 
-module "ip_plan" {
-  source = "./modules/ip-plan"
+# module "ip_plan" {
+#   source = "./modules/ip-plan"
 
-  fabric_cidr    = var.fabric_cidr
-  subnet_plan    = var.subnet_plan
-  switch_pair_id = var.switch_pair_name
-}
+#   fabric_cidr    = var.fabric_cidr
+#   subnet_plan    = var.subnet_plan
+#   switch_pair_id = var.switch_pair_name
+#}
