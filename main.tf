ba607d4c06260c49b078ad871d414f8925ad71a7
Craig Owen <craigeowen@gmail.com>
2026-03-09 15:47:58 +0000

    a

--- main.tf
+++ main.tf
@@ -2,55 +2,48 @@ terraform {
   required_providers {
     nxos = {
       source  = "CiscoDevNet/nxos"
-      version = "~> 0.5.10" # example, pin what you actually use
+      #version = "~> 0.8.0-beta7" # example, pin what you actually use
     }
   }
 }
 
+# locals {
+#   cfg = yamldecode(file("${path.module}/fabric.yaml"))
+# }
+
+
 locals {
-  cfg = yamldecode(file("${path.module}/fabric.yaml"))
+  devices = [
+    {
+      name    = "SAT-01"
+      url     = "https://192.168.1.166"
+      managed = true   # Actively managed
+    },
+    {
+      name    = "SAT-02"
+      url     = "https://192.168.1.144"
+      managed = true  # Temporarily freeze with false to halt use
+    },
+
+  ]
 }
 
+provider "nxos" {
+  username = "cisco"
+  password = "cisco"
+  devices  = local.devices
+}
 
-# Configure the Cisco NX-OS provider
-# provider "nxos" {
-#   alias    = "sat01"
-#   username = var.nxos_username
-#   password = var.nxos_password
-#   url      = var.switches.sat01_url
-# }
+resource "nxos_system" "hostname" {
+  for_each    = {for d in local.devices : d.name => d}
+  device      = each.key
+  name        = "${each.value.name}"
+ 
+}
 
-# provider "nxos" {
-#   alias    = "sat02"
-#   username = var.nxos_username
-#   password = var.nxos_password
-#   url      = var.switches.sat02_url
-# }
-
-# module "ip_plan" {
-#   source = "./modules/ip-plan"
-
-#   fabric_cidr    = var.fabric_cidr
-#   subnet_plan    = var.subnet_plan
-#   switch_pair_id = var.switch_pair_name
-#}
-
-module "n9k_pair" {
-  source = "./modules/n9k-pair"
-
-#   providers = {
-#     nxos.sat01 = nxos.sat01
-#     nxos.sat02 = nxos.sat02
-#   }
-
-  pair_name = var.switch_pair_name
-
-  # consume the derived IPs/subnets
-  loopbacks_sat01 = module.ip_plan.loopbacks_sat01
-  loopbacks_sat02 = module.ip_plan.loopbacks_sat02
-
-  svis_sat01 = module.ip_plan.svis_sat01
-  svis_sat02 = module.ip_plan.svis_sat02
-
-  ptp_links = module.ip_plan.ptp_links
+resource "nxos_vrf" "VRF1" {
+  for_each    = toset([for device in local.devices : device.name])
+  device      = each.key
+  name        = "VRF1"
+  description = "My VRF1 Description"
 }
