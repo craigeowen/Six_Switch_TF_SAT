96648bf725169cf3395237c8d49e73d982113789
Craig Owen <craigeowen@gmail.com>
2026-03-13 12:06:43 +0000

    smash down and rebuild

--- main.tf
+++ main.tf
@@ -7,21 +7,58 @@ terraform {
   }
 }
 
-locals {
-  cfg = yamldecode(file("./fabric.yaml"))
+provider "nxos" {
+  alias    = "LEAF-1"
+  username = "cisco"
+  password = "cisco"
+  url      = "https://192.168.1.166"
 }
 
 provider "nxos" {
+  alias    = "LEAF-2"
   username = "cisco"
   password = "cisco"
-  devices  = local.devices
+  url      = "https://192.168.1.144"
 }
 
-resource "nxos_system" "hostname" {
-  for_each     = local.cfg.devices
-  name         = each.value.name
+resource "nxos_vrf" "LEAF-1-VRF1" {
+  provider    = nxos.LEAF-1
+  name        = "VRF1"
+  description = "My VRF1 Description"
 }
 
+resource "nxos_vrf" "LEAF-2-VRF1" {
+  provider    = nxos.LEAF-2
+  name        = "VRF1"
+  description = "My VRF1 Description"
+}
+
+# Parses a YAML file into a Terraform-compatible map or object structure.
+  # 'file' reads the raw text from the disk.
+  # 'yamldecode' converts that text into data (lists, maps, strings).
+# locals {
+#   cfg = yamldecode(file("./fabric.yaml"))
+# }
+
+
+# Configures the system-level hostname for NX-OS devices
+
+#resource "nxos_system" "hostname" 
+
+  #for_each     = local.cfg.devices
+  # Iterates over a collection of devices defined in local.cfg.devices
+  # This allows you to manage multiple switches from a single resource block
+
+  #name         = each.value.name
+  # Sets the hostname ('name') based on the 'name' attribute of the current device in the loop
+
+
+
+# resource "nxos_system" "hostname" {
+#   for_each     = local.cfg.devices
+#   name         = each.value.name
+# }
+####################################################
 # locals {
 #   devices = [
 #     {
